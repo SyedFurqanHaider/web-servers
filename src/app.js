d03092cc6b2067daed102c6e5934cd63e0d6533f
Furqan Haider <furqan.haider@byteevo.com>
2021-12-28 09:32:22 -0800

    Added handle bars (hbs) and styling

--- src/app.js
+++ src/app.js
@@ -1,16 +1,64 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
+// Define paths for Express config.
 const publicDirPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialPath = path.join(__dirname, '../templates/partials');
 
 const app = express();
 
+// Setup handlebars engine and view location.
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialPath);
+
+// Setup static directory to serve.
 app.use(express.static(publicDirPath));
+
+app.get('', (req, res) => {
+  res.render('index', {
+    title: "Weather",
+    name: "Furqan Haider"
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about', {
+    title: "About Me",
+    name: "Furqan Haider"
+  });
+});
+
+app.get('/help', (req, res) => {
+  res.render('help', {
+    title: "Help",
+    message: "This page is for your help.",
+    name: "Furqan Haider"
+  });
+});
 
 app.get('/weather', (req, res) => {
   res.send({
     forecast: "Cloudy",
     location: "Islamabad",
+  });
+});
+
+app.get('/help/*', (req, res) => {
+  res.render('404', {
+    title: "404",
+    name: "Furqan Haider",
+    errorMessage: "Help article not found."
+  });
+});
+
+app.get('*', (req, res) => {
+  res.render('404', {
+    title: "404",
+    name: "Furqan Haider",
+    errorMessage: "Page not found."
   });
 });
 
